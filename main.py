fa705b48faabd21ce75a62ce84166f2c3e3d8967
pudy248 <lonnie.puddy@gmail.com>
2021-02-11 13:05:40 -0700

    finalized generation ?

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame as pg
-import time, sys, numpy, os
+import time, sys, os
 from multiprocessing.pool import Pool
 from noise import Noise
-from threading import  Thread
+from threading import Thread
 
 
 class Tile:
@@ -21,17 +21,11 @@ class Chunk:
                 t = Tile((self.pos[0] * CHUNKSIZE + x, self.pos[1] * CHUNKSIZE + y))
                 self.tiledict.update({(x, y): t})
 
-    def generate(self, CHUNKSIZE, N):
-        noise = numpy.zeros((N.AVG_RADIUS * 2 + CHUNKSIZE, N.AVG_RADIUS * 2 + CHUNKSIZE, 1))
-        print(time.perf_counter())
-        for x in range(noise.shape[0]):
-            for y in range(noise.shape[1]):
-                noise[x, y, 0] = N.layered_worley(x + N.AVG_RADIUS + (self.pos[0] * CHUNKSIZE), y + N.AVG_RADIUS + (self.pos[1] * CHUNKSIZE))
-        print(time.perf_counter())
-        for x in range(CHUNKSIZE):
-            for y in range(CHUNKSIZE):
+    def generate(self, chunksize, N):
+        for x in range(chunksize):
+            for y in range(chunksize):
                 s = self.tiledict[x, y]
-                s.id = 1 if N.average_cutoff((x +N.AVG_RADIUS, y + N.AVG_RADIUS), noise) > 0 else 0
+                s.id = 1 if N.interp_avg(x + N.AVG_RADIUS + (self.pos[0] * chunksize), y + N.AVG_RADIUS + (self.pos[1] * chunksize)) > N.AVG_CUTOFF else 0
         self.loaded = True
         print(time.perf_counter())
         return self
@@ -43,7 +37,7 @@ class Chunk:
                 for k in self.tiledict.keys():
                     surface.blit(sprites["tile_dark"] if self.tiledict[k].id == 0 else sprites["tile_light"], (k[0] * SCALING, k[1] * SCALING))
                 world.surfaces.update({self.pos: surface})
-            SURF.blit(world.surfaces[self.pos], ((self.pos[0] * CHUNKSIZE - player.pos[0]) * SCALING + int(W / 2), (self.pos[1] * CHUNKSIZE - player.pos[1]) * SCALING + int(H / 2)))
+            SURF.blit(world.surfaces[self.pos], ((self.pos[0] * CHUNKSIZE - player.world_pos[0]) * SCALING + int(W / 2), (self.pos[1] * CHUNKSIZE - player.world_pos[1]) * SCALING + int(H / 2)))
 
 
 class World:
@@ -55,32 +49,6 @@ class World:
         self.threads = {}
         self.global_tiledict = {}
         self.chunks.update({(0, 0): Chunk((0, 0))})
-
-    def noise_display(self):
-        arr_temp = numpy.zeros((W, H, 3))
-        threads = []
-        pool = Pool()
-        for y in range(0, H, 10):
-            threads.append(pool.apply_async(N.thread_f, (y, W)))
-        for t in threads:
-            r = t.get()
-            a = r[0]
-            diff = 10
-            if H - r[1] < 10:
-                diff = H - r[1]
-                a = r[0][0: r[0].shape[0], 0: H - r[1]]
-            arr_temp[0: W, r[1]: r[1] + diff, 0:3] = a
-            surfarr = pg.surfarray.make_surface(a)
-            SURF.blit(surfarr, (0, r[1]))
-        if N.AVERAGE:
-            threads2 = []
-            for y in range(H):
-                threads2.append(pool.apply_async(N.thread_avg, (y, arr_temp, W)))
-            print("average")
-            for t in threads2:
-                r = t.get()
-                surfarr = pg.surfarray.make_surface(r[0])
-                SURF.blit(surfarr, (0, r[1]))
 
     def loading_check(self):
         for c in self.chunks.values():
@@ -118,11 +86,11 @@ class World:
             if coords in self.surfaces.keys():
                 self.surfaces.pop(coords)
             for k in self.cached_chunks[coords].tiledict.keys():
-                self.global_tiledict.pop(self.cached_chunks[coords].tiledict[k].pos)
+                self.global_tiledict.pop(self.cached_chunks[coords].tiledict[k].world_pos)
 
 
     def chunks_loadingupdate(self):
-        center = (int(player.pos[0] / CHUNKSIZE), int(player.pos[1] / CHUNKSIZE))
+        center = (int(player.world_pos[0] / CHUNKSIZE), int(player.world_pos[1] / CHUNKSIZE))
         for x in range(center[0] - CHUNKLOAD_RADIUS - 1, center[0] + CHUNKLOAD_RADIUS + 1):
             for y in range(center[1] - CHUNKLOAD_RADIUS - 1, center[1] + CHUNKLOAD_RADIUS + 1):
                 if (x, y) not in self.chunks.keys():
@@ -133,28 +101,33 @@ class World:
                 or k[1] not in range(center[1] - CHUNKLOAD_RADIUS - 2, center[1] + CHUNKLOAD_RADIUS + 2):
                 self.unload_chunk(k)
 
-#data format: (pos, lifetime, image str, team
+#data format: (pos, lifetime, image str, team)
 class PhysicsEntity(pg.sprite.Sprite):
     def __init__(self, data):
         pg.sprite.Sprite.__init__(self)
-        self.pos = data[0]
+        self.world_pos = data[0]
         self.vel = [0, 0]
+        self.vectors = []
         self.lifetime = data[1]
         self.image = sprites[data[2]]
         self.team = data[3]
 
     def update(self):
         mult = 1 / (sum(fpsArr) / len(fpsArr))
+        if self.lifetime != -1:
+            self.lifetime -= mult
+            if self.lifetime < 0:
+                projectileGroup.remove(self)
         self.vel[1] += 40 * mult
         self.vector_recalc()
         if len(self.vectors) > 0:
             if [0, 0] in self.vectors:
-                self.pos[1] -= 1
+                self.world_pos[1] -= 1
             if [0, 1] in self.vectors:
                 while [0, 1] in self.vectors:
-                    self.pos[1] -= 0.006
+                    self.world_pos[1] -= 0.006
                     self.vector_recalc()
-                self.pos[1] += 0.02
+                self.world_pos[1] += 0.02
                 self.vector_recalc()
                 self.vel[1] = 0
                 if (self.vel[0] < 0 and pg.key.get_pressed()[pg.K_d]) or (self.vel[0] > 0 and pg.key.get_pressed()[pg.K_a]) or (not pg.key.get_pressed()[pg.K_a] and not pg.key.get_pressed()[pg.K_d]):
@@ -163,44 +136,44 @@ class PhysicsEntity(pg.sprite.Sprite):
                     self.vel[0] /= 1 + (3 * mult)
             if [0, -1] in self.vectors:
                 self.vel[1] /= -2
-                self.pos[1] += 0.1
+                self.world_pos[1] += 0.1
             if [1, 0] in self.vectors:
                 self.vel[0] /= -2
                 while [1, 0] in self.vectors:
-                    self.pos[0] -= 0.006
+                    self.world_pos[0] -= 0.006
                     self.vector_recalc()
             if [-1, 0] in self.vectors:
                 self.vel[0] /= -2
                 while [-1, 0] in self.vectors:
-                    self.pos[0] += 0.006
+                    self.world_pos[0] += 0.006
                     self.vector_recalc()
         else:
             self.vel[0] /= 1 + (.25 * mult)
-        self.pos[0] += self.vel[0] * mult
-        self.pos[1] += self.vel[1] * mult
+        self.world_pos[0] += self.vel[0] * mult
+        self.world_pos[1] += self.vel[1] * mult
 
     def vector_recalc(self):
         tolerance = 0
         self.vectors = []
-        xb = round(self.pos[0])
-        yb = round(self.pos[1])
+        xb = round(self.world_pos[0])
+        yb = round(self.world_pos[1])
         for x in range(xb - 1, xb + 2):
             for y in range(yb - 1, yb + 2):
-                if xb - self.pos[0] < -tolerance and x - xb == -1:
+                if xb - self.world_pos[0] < -tolerance and x - xb == -1:
                     continue
-                if xb - self.pos[0] > tolerance and x - xb == 1:
+                if xb - self.world_pos[0] > tolerance and x - xb == 1:
                     continue
-                if yb - self.pos[1] < -tolerance and y - yb == -1:
+                if yb - self.world_pos[1] < -tolerance and y - yb == -1:
                     continue
-                if yb - self.pos[1] > tolerance and y - yb == 1:
+                if yb - self.world_pos[1] > tolerance and y - yb == 1:
                     continue
                 if (x, y) in world.global_tiledict and world.global_tiledict[(x, y)].id == 0:
                     self.vectors.append([x - xb, y - yb])
 
-class Player(PhysicsEntity):
+class Player (PhysicsEntity):
     def __init__(self, data):
         PhysicsEntity.__init__(self, data)
-        self.pos = [CHUNKSIZE / 2, CHUNKSIZE / 2]
+        self.world_pos = [CHUNKSIZE / 2, CHUNKSIZE / 2]
         self.vectors = []
 
     def update(self):
@@ -211,7 +184,7 @@ class Player(PhysicsEntity):
             self.vel[0] += 40 * mult
         if pg.key.get_pressed()[pg.K_SPACE] and [0, 1] in self.vectors:
             player.vel[1] -= 50
-            player.pos[1] -= .05
+            player.world_pos[1] -= .05
         PhysicsEntity.update(self)
 
 
@@ -226,10 +199,9 @@ if __name__ == '__main__':
 
     ##### GAMEPLAY PARAMS #####
     FPS = 120
-    NOISE_TESTING_MODE = False
-    CHUNKLOAD_RADIUS = 2
+    CHUNKLOAD_RADIUS = 3
     CHUNKSIZE = 128
-    SCALING = 4
+    SCALING = 2
 
     sprites = {
         "tile_dark": pg.transform.scale(pg.image.load("sprites\\tile_dark.png"), (SCALING, SCALING)),
@@ -242,13 +214,13 @@ if __name__ == '__main__':
     timer = 0
     initial_load = False
     dt = 0
+
+    projectileGroup = pg.sprite.Group()
+
     N = Noise()
     world = World()
-    if NOISE_TESTING_MODE:
-        thread = Thread(target=world.noise_display)
-    else:
-        player = Player(([CHUNKSIZE / 2, CHUNKSIZE / 2], -1, "tile_blue", 0))
-        thread = Thread(target=world.load_all)
+    player = Player(([CHUNKSIZE / 2, CHUNKSIZE / 2], -1, "tile_blue", 0))
+    thread = Thread(target=world.load_all)
     thread.start()
     while True:
         timer = time.perf_counter()
@@ -258,44 +230,24 @@ if __name__ == '__main__':
                 sys.exit()
             if event.type == pg.KEYDOWN:
                 k = event.key
-                if not thread.is_alive() and k == pg.K_BACKQUOTE and NOISE_TESTING_MODE:
-                    s = input("s:")
-                    N.SCALE = N.SCALE if s == "" else int(s)
-                    o = input("o:")
-                    N.OCTAVES = N.OCTAVES if o == "" else int(o)
-                    p = input("p:")
-                    N.PERSISTENCE = N.PERSISTENCE if p == "" else float(p)
-                    f = input("f:")
-                    N.FRACTAL_RATIO = N.FRACTAL_RATIO if f == "" else float(f)
-                    b = input("b:")
-                    N.SIGMOID_B = N.SIGMOID_B if b == "" else int(b)
-                    so = input("so:")
-                    N.SIGMOID_OFFSET = N.SIGMOID_OFFSET if so == "" else float(so)
-                    if N.AVERAGE:
-                        r = input("r:")
-                        N.AVG_RADIUS = N.AVG_RADIUS if r == "" else int(r)
-                        c = input("c:")
-                        N.AVG_CUTOFF = N.AVG_CUTOFF if c == "" else int(c)
-                        e = input("e:")
-                        N.AVG_EFFECT = N.AVG_EFFECT if e == "" else float(e)
-                    thread = Thread(target=world.noise_display)
-                    thread.start()
-                    print(N.SCALE, N.OCTAVES, N.PERSISTENCE, N.FRACTAL_RATIO, N.SEED, N.SIGMOID_B, N.SIGMOID_OFFSET,
-                          N.AVERAGE, N.AVG_RADIUS, N.AVG_CUTOFF, N.AVG_EFFECT)
-        if not NOISE_TESTING_MODE:
-            initial_load = initial_load or world.loading_check()
-            SURF.fill((0, 0, 0))
-            if initial_load:
-                if dt > 5:
-                    world.chunks_loadingupdate()
-                    dt = 0
-                for c in world.chunks.values():
-                    c.draw()
-                player.update()
-                SURF.blit(sprites["tile_blue"], (int(W / 2), int(H / 2)))
-            f = pg.font.SysFont("Arial", 15)
-            r = f.render(str(int(sum(fpsArr) / len(fpsArr))), True, pg.Color("red"))
-            SURF.blit(r, (5, 5))
+
+
+        SURF.fill((0, 0, 0))
+        initial_load = initial_load or world.loading_check()
+        if initial_load:
+            if dt > 5:
+                world.chunks_loadingupdate()
+                dt = 0
+            for c in world.chunks.values():
+                c.draw()
+            projectileGroup.update()
+            projectileGroup.draw(SURF)
+            player.update()
+            SURF.blit(sprites["tile_blue"], (int(W / 2), int(H / 2)))
+        f = pg.font.SysFont("Arial", 15)
+        r = f.render(str(int(sum(fpsArr) / len(fpsArr))), True, pg.Color("red"))
+        SURF.blit(r, (5, 5))
+
         pg.display.update()
         clock.tick(FPS)
         dt += (time.perf_counter() - timer)
--- noise.py
+++ noise.py
@@ -5,21 +5,22 @@ import math, random, numpy
 class Noise:
     def __init__(self):
         self.SCALE = 60
-        self.OCTAVES = 8
-        self.PERSISTENCE = .6
-        self.FRACTAL_RATIO = 1.7
-        self.SEED = 34575334
+        self.OCTAVES = 7
+        self.PERSISTENCE = .55
+        self.FRACTAL_RATIO = 1.65
+        self.SEED = int(random.random() * 1000000)
 
-        self.SIGMOID_B = .001
+        self.SIGMOID_B = 3
         self.SIGMOID_OFFSET = 0.0
 
         self.AVERAGE = True
-        self.AVG_RADIUS = 10
-        self.AVG_CUTOFF = 105
+        self.AVG_RADIUS = 20
+        self.AVG_CUTOFF = 125
         self.AVG_EFFECT = 1
 
-        self.interp_scale = 10
+        self.interp_scale = 8
         self.points = {}
+        self.points_avg = {}
 
     def worley(self, tp):
         x1 = int(tp[0])
@@ -39,57 +40,29 @@ class Noise:
         return min(distance)
 
     def layered_worley(self, xi, yi):
-        if xi % self.interp_scale == 0 and yi % self.interp_scale == 0:
-            if (xi, yi) not in self.points.keys():
-                x = xi / self.SCALE / self.interp_scale
-                y = yi / self.SCALE / self.interp_scale
-                b = 1
-                t = 0
-                for i in range(self.OCTAVES):
-                    offset = self.SEED * (i + 1)
-                    t += self.worley((x * numpy.power(self.FRACTAL_RATIO, i) + offset, y * numpy.power(self.FRACTAL_RATIO, i) + offset)) * numpy.power(self.PERSISTENCE, i)
-                    b += numpy.power(self.PERSISTENCE, i)
-                self.points.update({(xi, yi): self.sigmoid(t / b + self.SIGMOID_OFFSET) * 255})
-            return self.points[(xi, yi)]
-        else:
-            xr = (xi % self.interp_scale) / self.interp_scale
-            yr = (yi % self.interp_scale) / self.interp_scale
-            if (xi - (xi % self.interp_scale), yi - (yi % self.interp_scale)) not in self.points.keys():
-                p1 = self.layered_worley(xi - (xi % self.interp_scale), yi - (yi % self.interp_scale))
-            else:
-                p1 = self.points[(xi - (xi % self.interp_scale), yi - (yi % self.interp_scale))]
-            if (xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale)) not in self.points.keys():
-                p2 = self.layered_worley(xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale))
-            else:
-                p2 = self.points[(xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale))]
-            if (xi - (xi % self.interp_scale), yi - (yi % self.interp_scale) + self.interp_scale) not in self.points.keys():
-                p3 = self.layered_worley(xi - (xi % self.interp_scale), yi - (yi % self.interp_scale) + self.interp_scale)
-            else:
-                p3 = self.points[(xi - (xi % self.interp_scale), yi - (yi % self.interp_scale) + self.interp_scale)]
-            if (xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale) + self.interp_scale) not in self.points.keys():
-                p4 = self.layered_worley(xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale) + self.interp_scale)
-            else:
-                p4 = self.points[(xi - (xi % self.interp_scale) + self.interp_scale, yi - (yi % self.interp_scale) + self.interp_scale)]
-            if xi % self.interp_scale != 0:
-                if yi % self.interp_scale != 0:
-                    return (p4 * xr * yr) + (p3 * (1 - xr) * yr) + (p2 * (1 - yr) * xr) + (p1 * (1 - xr) * (1 - yr))
-                else:
-                    return (p1 * (1 - xr)) + (p2 * xr)
-            else:
-                if yi % self.interp_scale != 0:
-                    return (p1 * (1 - yr)) + (p3 * yr)
+        if (xi, yi) not in self.points.keys():
+            x = (xi / self.SCALE) / self.interp_scale
+            y = (yi / self.SCALE) / self.interp_scale
+            b = 1
+            t = 0
+            for i in range(self.OCTAVES):
+                offset = self.SEED * (i + 1)
+                t += self.worley((x * numpy.power(self.FRACTAL_RATIO, i) + offset,
+                                  y * numpy.power(self.FRACTAL_RATIO, i) + offset)) * numpy.power(self.PERSISTENCE, i)
+                b += numpy.power(self.PERSISTENCE, i)
+            self.points.update({(xi, yi): self.sigmoid(t / b + self.SIGMOID_OFFSET) * 255})
+        return self.points[(xi, yi)]
 
+    def interp_avg(self, x, y):
+        xr = (x % self.interp_scale) / self.interp_scale
+        yr = (y % self.interp_scale) / self.interp_scale
 
-    def set_pixel(self, x, y, arr):
-        a = self.layered_worley(x, y)
-        arr[x, y % 10, 0: 3] = [a] * 3
+        p1 = self.pixel_avg(x - (x % self.interp_scale), y - (y % self.interp_scale))
+        p2 = self.pixel_avg(x - (x % self.interp_scale) + self.interp_scale, y - (y % self.interp_scale))
+        p3 = self.pixel_avg(x - (x % self.interp_scale), y - (y % self.interp_scale) + self.interp_scale)
+        p4 = self.pixel_avg(x - (x % self.interp_scale) + self.interp_scale, y - (y % self.interp_scale) + self.interp_scale)
 
-    def thread_f(self, y, W):
-        arr = numpy.zeros((W, 10, 3))
-        for y1 in range(10):
-            for x in range(int(W)):
-                self.set_pixel(x, y+y1, arr)
-        return arr, y
+        return (p4 * xr * yr) + (p3 * (1 - xr) * yr) + (p2 * (1 - yr) * xr) + (p1 * (1 - xr) * (1 - yr))
 
     def sigmoid(self, x):
         if self.SIGMOID_B <= 1:
@@ -97,25 +70,17 @@ class Noise:
         else:
             return .5 * (1 / numpy.sinh(self.SIGMOID_B / 4)) * (1 / numpy.cosh(.25 * (self.SIGMOID_B - (2 * self.SIGMOID_B * x)))) * numpy.sinh(self.SIGMOID_B * x / 2)
 
-    def average_cutoff(self, center, source):
-        if self.AVERAGE:
-            arr = numpy.zeros((self.AVG_RADIUS * 2, self.AVG_RADIUS * 2))
-            for x in range(self.AVG_RADIUS * 2):
-                for y in range(self.AVG_RADIUS * 2):
-                    if 0 <= x + center[0] - self.AVG_RADIUS < source.shape[0] and 0 <= y + center[1] - self.AVG_RADIUS < source.shape[1]:
-                        arr[x, y] = source[x + center[0] - self.AVG_RADIUS, y + center[1] - self.AVG_RADIUS, 0]
-            t = sum(arr.flatten())
-            div = (self.AVG_RADIUS ** 2) * 4
-            if source[center[0], center[1], 0] >= ((t / div) - self.AVG_CUTOFF) * self.AVG_EFFECT + self.AVG_CUTOFF:
-                return 255
+    def pixel_avg(self, xi, yi):
+        if (xi, yi) not in self.points_avg.keys():
+            if self.AVERAGE:
+                arr = numpy.zeros((self.AVG_RADIUS * 2, self.AVG_RADIUS * 2))
+                for x in range(self.AVG_RADIUS * 2):
+                    for y in range(self.AVG_RADIUS * 2):
+                        arr[x, y] = self.layered_worley(x + xi - self.AVG_RADIUS, y + yi - self.AVG_RADIUS)
+                t = sum(arr.flatten())
+                div = (self.AVG_RADIUS ** 2) * 4
+                a = self.layered_worley(xi, yi) + (((t / div) - self.AVG_CUTOFF) * self.AVG_EFFECT)
             else:
-                return 0
-        else:
-            return 255 if source[center[0], center[1], 0] > self.AVG_CUTOFF else 0
-
-    def thread_avg(self, y, arr, W):
-        arr2 = numpy.zeros((W, 1, 3))
-        for x in range(W):
-            arr2[x, 0, 0:3] = [self.average_cutoff((x, y), arr)] * 3
-        return arr2, y
-
+                a = self.layered_worley(xi, yi)
+            self.points_avg.update({(xi, yi): a})
+        return self.points_avg[(xi, yi)]
